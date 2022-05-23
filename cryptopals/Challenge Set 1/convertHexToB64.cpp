bf72e9223385dccf4e16977c2efedf64507c836a
nji0 <mossnick45@gmail.com>
2022-05-23 11:32:17 +1000

    cpp commit

---
+++ cryptopals/Challenge Set 1/convertHexToB64.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+// input in hex: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+// output in base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+std::string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+int hexToRaw(std::string hex) {
+    
+}
+
+std::string convertHexToB64(int hex) {
+    
+}
+
+int main(int argc, char** argv) {
+    std::cout << "test" << std::endl;
+}
