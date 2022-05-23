43df4ab8c39cee76d9b4eaf13383f494581f9ea2
nji0 <mossnick45@gmail.com>
2022-05-23 11:31:54 +1000

    inital commit of python and cpp files, both bare bones

---
+++ cryptopals/Challenge Set 1/convertHexToB64.py
@@ -0,0 +1,9 @@
+# input in hex: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# output in base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+
+def main():
+    print("test")
+
+if __name__ == "__main__":
+    main()
