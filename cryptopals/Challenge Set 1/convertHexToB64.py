ab775b521f49d9f31532c7a154e775dccdeaa72f
nji0 <mossnick45@gmail.com>
2022-05-30 17:28:12 +1000

    python solution using codecs

--- cryptopals/Challenge Set 1/convertHexToB64.py
+++ cryptopals/Challenge Set 1/convertHexToB64.py
@@ -1,23 +1,18 @@
 # input in hex: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 # output in base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
+import codecs
+
 
 def hexToBinaryString(hex):
-    binString = ""
-
-    l = len(hex)
-    
-    for x in range(0, l):
-        binString+=(hex[-1-x])
-
-    print(binString)
+    return codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
 
 
 def main():
 
     input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-    hexToBinaryString(input)
+    print(hexToBinaryString(input))
 
 if __name__ == "__main__":
     main()
