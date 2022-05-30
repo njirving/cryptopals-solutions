af58d719759bcfb289bcd463445da6c316a300b3
nji0 <mossnick45@gmail.com>
2022-05-30 17:36:18 +1000

    char to int done in cpp solution

--- cryptopals/Challenge Set 1/convertHexToB64.cpp
+++ cryptopals/Challenge Set 1/convertHexToB64.cpp
@@ -6,6 +6,20 @@
 
 std::string input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
+
+int charToInt(char c) {
+    
+    if(c >= '0' && c <= '9')
+        return c - '0';
+    if(c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
+    if(c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    
+    throw std::invalid_argument("Invalid input");
+}
+
+
 int hexToRaw(std::string hex) {
     
 }
