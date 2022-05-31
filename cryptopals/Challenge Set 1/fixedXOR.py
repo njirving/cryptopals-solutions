dd6018efa2716c3c5f1968ed7610bd10c454d0e9
nji0 <mossnick45@gmail.com>
2022-05-31 15:01:23 +1000

    better xor

--- cryptopals/Challenge Set 1/fixedXOR.py
+++ cryptopals/Challenge Set 1/fixedXOR.py
@@ -2,17 +2,21 @@ import codecs
 import sys
 
 #test values
-hex = "1c0111001f010100061a024b53535009181c"
-xor = "686974207468652062756c6c277320657965"
+#hex = "1c0111001f010100061a024b53535009181c"
+#xor = "686974207468652062756c6c277320657965"
 
 #xor function
-def fixedXOR(hex, xor):
-    return codecs.encode(bytes((a ^ b) for a, b in zip(codecs.decode(hex, 'hex'), codecs.decode(xor, 'hex'))), 'hex').decode()
+def fixedXOR(hexi, xor):
+    return codecs.encode(bytes((a ^ b) for a, b in zip(codecs.decode(hexi, 'hex'), codecs.decode(xor, 'hex'))), 'hex').decode()
+
+#perhaps a better xor function
+def bFixedXOR(hexi, xor):
+    return hex(int(hexi, 16) ^ int(xor, 16))[2:]
 
 #main block
 def main():
     if len(sys.argv) == 3:
-        print(fixedXOR(sys.argv[1], sys.argv[2]))
+        print(bFixedXOR(sys.argv[1], sys.argv[2]))
     else:
         print("Usage: fixedXOR.py [hex string] [xor string]")
 
