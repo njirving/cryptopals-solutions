bab1a0095eb8765d5e3198fbba3524584c8ff082
nji0 <mossnick45@gmail.com>
2022-06-01 19:04:50 +1000

    try except block to push past non utf-8 characters

--- cryptopals/Challenge Set 1/singleByteXORCipher.py
+++ cryptopals/Challenge Set 1/singleByteXORCipher.py
@@ -15,7 +15,10 @@ def singleCharXOR(h, c):
 def main():
     h = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
     for i in range(0,255):
-        print(singleCharXOR(h, i))
+        try:
+            print(singleCharXOR(h, i))
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
